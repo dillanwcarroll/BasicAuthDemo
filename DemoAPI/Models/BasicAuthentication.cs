8f8a8c92afd544b6b716c6533e07a25897db7dfe
Dillan Carroll <Spaced92@gmail.com>
2018-09-10 12:42:36 +1000

    Add project files.

---
+++ DemoAPI/Models/BasicAuthentication.cs
@@ -0,0 +1,47 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace DemoAPI.Models
+{
+    public class BasicAuthentication : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                //get the Hear values
+                string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                //Decoded the authenticationToken values because client passed the user name and password in encoded form
+                string decodedAuthenticationToken = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                //Split the username and password by : because the client passed the username and password as "userNameValue:Passwordvalue"
+                string[] usernamePasswordArray = decodedAuthenticationToken.Split(':');
+                string username = usernamePasswordArray[0];
+                string password = usernamePasswordArray[1];
+                UserManager _userRepository = new UserManager();
+
+                //validate from the database for this username or password
+                if (_userRepository.ValidateUser(username, password))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), roles: null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+        //Check client passed any value in header or not
+        
+    }
+}
